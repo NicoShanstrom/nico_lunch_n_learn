786b04c293333890cd0b06a7badd717734ee9b66
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-06-08 12:11:24 -0400

    Initial setup of youtube api search.list request with channelId

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -1,18 +1,15 @@
 class YoutubeService
-  def self.search(query_keywords, video_duration)
+  def self.search(query_keywords)
     url = 'search'
     params = {
       part: 'snippet',
-      maxResults: 25,
-      order: 'relevance',
-      type: 'video',
+      maxResults: 1,
       videoEmbeddable: true,
       q: query_keywords,
-      videoDuration: video_duration
+      channel_id: UCluQ5yInbeAkkeCndNnUhpw
     }
 
     response = call_api(url, params)
-    # require 'pry'; binding.pry
     parse_response(response)
   end
 
@@ -21,7 +18,7 @@ class YoutubeService
   def self.call_api(url, params = {})
     response = connection.get(url) do |request|
       request.params = params
-      request.params[:key] = Rails.application.credentials.google[:api_key]
+      request.params[:key] = Rails.application.credentials.YOUTUBE[:API_KEY]
     end
 
     JSON.parse(response.body, symbolize_names: true)
@@ -42,11 +39,8 @@ class YoutubeService
         YoutubeVideo.new(
           id: { videoId: item[:id][:videoId] },
           snippet: item[:snippet],
-          contentDetails: item[:contentDetails],
           title: item[:snippet][:title],
           url: "https://www.youtube.com/watch?v=#{item[:id][:videoId]}",
-          duration: item[:contentDetails]&.dig(:duration), 
-          thumbnail_url: item[:snippet][:thumbnails][:default][:url]
         )
       end
     end
