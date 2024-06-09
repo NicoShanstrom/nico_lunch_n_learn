b5a78a4fab641d5b217c8274641cba8d5fdc32a4
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-06-08 21:52:50 -0400

    Adjusts learning resources serializer after poro for attributes only

--- app/serializers/learning_resource_serializer.rb
+++ app/serializers/learning_resource_serializer.rb
@@ -2,14 +2,6 @@ class LearningResourceSerializer
   include JSONAPI::Serializer
 
   set_id { nil }
-  
+
   attributes :country, :video, :images
-
-  attribute :video do |object|
-    object[:attributes][:video]
-  end
-
-  attribute :images do |object|
-    object[:attributes][:images]
-  end
 end
