f8ef3741445363bc3983fd4ffc9fa84be54133ba
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-06-10 12:09:03 -0400

    Remembers to instantiate country_name in touristsitescontroller

--- app/controllers/api/v1/tourist_sites_controller.rb
+++ app/controllers/api/v1/tourist_sites_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::TouristSitesController < ApplicationController
   def index
-    if country.present?
-      tourist_sites = PlacesService.tourist_sites(country)
+    country_name = params[:county_name]
+    if country_name.present?
+      tourist_sites = PlacesService.tourist_sites(country_name)
       render json: tourist_sites
     else
       render json: { error: "Country parameter is required for search" }
+    end
   end
 end
