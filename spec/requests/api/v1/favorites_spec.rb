5bd79f8dbd83489b5f67001d518808c606077128
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-06-11 12:25:18 -0400

    create another user in favotires spec, creates sad path tests for get favorites spec

--- spec/requests/api/v1/favorites_spec.rb
+++ spec/requests/api/v1/favorites_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe "Favorite request", type: :request do
     ) 
   }
 
+  let(:user2) { 
+    User.create(
+      name: "Wolf", email: "test1@example.com", 
+      password: "password1", password_confirmation: "password1"
+    ) 
+  }
+
   before do
     user.favorites.create(
       country: "thailand",
@@ -20,7 +27,7 @@ RSpec.describe "Favorite request", type: :request do
       recipe_title: "Recipe: Egyptian Tomato Soup"
     )
   end
-  
+
   describe "POST /api/v1/favorites" do
     describe "successful post favorite request" do
       it "creates a favorite recipe for a user" do
@@ -65,27 +72,61 @@ RSpec.describe "Favorite request", type: :request do
   describe "GET /api/v1/favorites" do
     describe "successful get request for a user's favorites" do
       it "returns a users favorite recipes" do
-        get "/api/v1/favorites?api_key=#{user.api_key}"
+        get "/api/v1/favorites?api_key=#{user.api_key}",
           headers: {
             "Content-Type" => "application/json",
             "Accept" => "application/json"
           }
 
-        expect(response).to have_http_status(:success)
+        expect(response).to have_http_status(:ok)
         json_response = JSON.parse(response.body, symbolize_names: true)
         expect(json_response).to be_a(Hash)
         expect(json_response[:data]).to be_an(Array)
-        expect(json_response[:data]).to have_key(:id)
-        expect(json_response[:data]).to have_key(:type)
-        expect(json_response[:data][:type]).to be_a(String)
 
-        expect(json_response[:data]).to have_key(:attributes)
-        expect(json_response[:data][:attributes]).to be_a(Hash)
-        expect(json_response[:data][:attributes]).to have_key(:recipe_title)
-        expect(json_response[:data][:attributes]).to have_key(:recipe_link)
-        expect(json_response[:data][:attributes]).to have_key(:country)
-        expect(json_response[:data][:attributes][:country]).to be_a(String)
-        expect(json_response[:data][:attributes]).to have_key(:created_at)
+        favorite = json_response[:data].first
+        expect(favorite).to have_key(:id)
+        expect(favorite[:id]).to be_a(String)
+
+        expect(favorite).to have_key(:type)
+        expect(favorite[:type]).to eq("favorite")
+
+        expect(favorite).to have_key(:attributes)
+        expect(favorite[:attributes]).to be_a(Hash)
+        expect(favorite[:attributes]).to have_key(:recipe_title)
+        expect(favorite[:attributes]).to have_key(:recipe_link)
+        expect(favorite[:attributes]).to have_key(:country)
+        expect(favorite[:attributes][:country]).to be_a(String)
+        expect(favorite[:attributes]).to have_key(:created_at)
+      end
+    end
+
+    describe "unsuccessful request" do
+      it "returns an error when API key is invalid" do
+        get "/api/v1/favorites?api_key=newenglandclamchowder", 
+          headers: {
+            "Content-Type" => "application/json",
+            "Accept" => "application/json"
+          }
+
+        expect(response).to have_http_status(:bad_request)
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        expect(json_response[:error]).to eq("Invalid API key")
+      end
+    end
+
+    describe "no favorites" do
+      it "returns an empty array when user has no favorites" do
+        get "/api/v1/favorites?api_key=#{user2.api_key}", 
+            headers: {
+              "Content-Type" => "application/json",
+              "Accept" => "application/json"
+            }
+        
+        expect(response).to have_http_status(:ok)
+        json_response = JSON.parse(response.body, symbolize_names: true)
+        expect(json_response).to be_a(Hash)
+        expect(json_response[:data]).to be_an(Array)
+        expect(json_response[:data]).to be_empty
       end
     end
   end
